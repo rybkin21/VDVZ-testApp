0912b2f3e53db6f3113221eea75456b764cd3dfa
ivanrybkin <rybkin2517@mail.ru>
2024-08-27 15:08:34 +0300

    complete settings project & added appIcon, launchScreen image

--- VDVZ-testApp/MainViewController.swift
+++ VDVZ-testApp/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  VDVZ-testApp
 //
 //  Created by Ivan Rybkin on 27.08.2024.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
     }
 
 
